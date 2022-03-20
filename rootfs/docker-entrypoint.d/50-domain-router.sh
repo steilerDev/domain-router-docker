c65760885597123d497df4dcaba68065f0d48478
Frank Steiler <frank@steilerdev.de>
2022-03-20 16:03:08 +0000

    Testing

--- rootfs/docker-entrypoint.d/50-domain-router.sh
+++ rootfs/docker-entrypoint.d/50-domain-router.sh
@@ -3,8 +3,6 @@
 TMPL_FILE="/opt/domain-router/domain.conf.tmpl"
 NGINX_CONF="/etc/nginx/conf.d"
 
-export HOSTS=""
-
 function createRoute {
     echo "  - Creating route $1 from $2 to $3"
 
@@ -31,12 +29,12 @@ compgen -A variable | grep -E "^ROUTER_" | while read line; do
     IFS=';' read -ra SOURCES <<< ${RULE[0]}
     for source in "${SOURCES[@]}"; do
         createRoute "${NAME}-${index}" $source $DEST
-        HOSTS+=",$source"
+        export HOSTS+=",$source"
         echo $HOSTS
         index=$((index + 1))
     done
 done
-
+echo $HOSTS
 export VIRTUAL_HOST=${HOSTS:1}
 export LETSENCRYPT_HOST=${HOSTS:1}
 export VIRTUAL_PORT=80
